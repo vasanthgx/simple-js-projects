0d64ddfcad72104c0c8a6ff8f826fd3a86cf2c51
Vasanth Gopal <vasanth1627@gmail.com>
2024-07-11 15:07:12 +0530

    updated index.js file

--- projects/stopwatch/index.js
+++ projects/stopwatch/index.js
@@ -17,5 +17,35 @@ function startTimer(){
 }
 
 function formatTime(elapsedTime){
+    const milliseconds = Math.floor((elapsedTime %1000)/10);
+    const seconds = Math.floor((elapsedTime %(1000 * 60)) / 1000);
+    const minutes = Math.floor((elapsedTime % 1000 * 60 * 60) / (1000 * 60));
+    const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+
+    return (
+        (hours ? (hours > 9 ? hours : '00' + hours) : '00' ) + ":" +
+        (minutes ? (minutes > 9 ? minutes : "00" + minutes) : '00') + ":" +
+        (seconds ? (seconds > 9 ? seconds : "00" + seconds) : '00') + ":" +
+        (milliseconds > 9 ? milliseconds : '00' + milliseconds)
+
+    );
     
 }
+
+function stopTimer(){
+    clearInterval(timerInterval);
+    startButtonEl.disabled = false;
+    stopButtonEl.disabled = true;
+}
+
+function resetTimer(){
+    clearInterval(timerInterval);
+    elapsedTime = 0;
+    timerEl = "00:00:00";
+    startButtonEl.disabled = false;
+    stopButtonEl.disabled = true;
+}
+
+startButtonEl.addEventListener('click', startTimer);
+stopButtonEl.addEventListener('click', stopTimer);
+resetButtonEl.addEventListener('click', resetTimer);
